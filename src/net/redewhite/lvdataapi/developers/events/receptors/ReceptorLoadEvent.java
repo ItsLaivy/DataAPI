4c9692b3d7ac24c47f21e5b331b0918d1a48edbd
BuildTools <unconfigured@null.spigotmc.org>
2022-01-17 11:33:55 -0300

    3.5-3

--- src/net/redewhite/lvdataapi/developers/events/receptors/ReceptorLoadEvent.java
+++
@@ -1,9 +0,0 @@
-package net.redewhite.lvdataapi.developers.events.receptors;
-
-import net.redewhite.lvdataapi.modules.ReceptorCreator;
-
-public class ReceptorLoadEvent extends ReceptorEvent {
-    public ReceptorLoadEvent(boolean isAsync, ReceptorCreator receptor) {
-        super(isAsync, receptor);
-    }
-}
--- src/net/redewhite/lvdataapi/developers/events/receptors/ReceptorSaveEvent.java
+++
@@ -1,9 +0,0 @@
-package net.redewhite.lvdataapi.developers.events.receptors;
-
-import net.redewhite.lvdataapi.modules.ReceptorCreator;
-
-public class ReceptorSaveEvent extends ReceptorEvent {
-    public ReceptorSaveEvent(boolean isAsync, ReceptorCreator receptor) {
-        super(isAsync, receptor);
-    }
-}
